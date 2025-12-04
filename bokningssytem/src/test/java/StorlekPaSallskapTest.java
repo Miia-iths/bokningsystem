e86d7a8efeefa0b89554e19694315b3a50ce8548
Filip <isberget23@gmail.com>
2025-12-04 14:49:18 +0100

    Kommenterade ut första testet vi gjorde

--- bokningssytem/src/test/java/StorlekPaSallskapTest.java
+++ bokningssytem/src/test/java/StorlekPaSallskapTest.java
@@ -1,15 +1,18 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class StorlekPaSallskapTest {
-
-    @Test
-
-    public void testStorlekPaSallskapGiltig() {
-        StorlekPaSallskap sps = new StorlekPaSallskap();
-        int storlek = 5; // Exempelstorlek //Math random mellan, för att testa ett nytt senario flera
-                         // gånger, kom ihåg att lägga plus 1 math börjar på 0
-        assertTrue(storlek > 0 && storlek <= 12);
-
-    }
-}
+/*
+ * public class StorlekPaSallskapTest {
+ * 
+ * @Test
+ * 
+ * public void testStorlekPaSallskapGiltig() {
+ * StorlekPaSallskap sps = new StorlekPaSallskap();
+ * int storlek = 5; // Exempelstorlek //Math random mellan, för att testa ett
+ * nytt senario flera
+ * // gånger, kom ihåg att lägga plus 1 math börjar på 0
+ * assertTrue(storlek > 0 && storlek <= 12);
+ * 
+ * }
+ * }
+ */
